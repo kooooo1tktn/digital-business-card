cadea0b4d4ba92efa0b0d77a1bfe39ca9df1905b
Kouichi Taketani <4649human@gmail.com>
2025-04-23 02:12:27 +0900

    add: 登録画面のUIを改善し、フォームコンポーネントを追加

--- src/register.tsx
+++ src/register.tsx
@@ -1,9 +1,45 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+} from "@chakra-ui/react";
+
 export const Register = () => {
   return (
     <>
-      <div>
-        <h1>新規名刺登録</h1>
-      </div>
+      <Heading mb={4}>新規名刺登録</Heading>
+      <Box p={4} borderWidth={1} borderRadius="lg">
+        <FormControl isRequired>
+          <FormLabel fontWeight={"bold"}>お名前</FormLabel>
+          <Input type="name" placeholder="山田 太郎" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel fontWeight={"bold"}>自己紹介</FormLabel>
+          <Textarea isInvalid placeholder="よろしくお願いします" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel fontWeight={"bold"}>好きな技術</FormLabel>
+          <Select placeholder="好きな技術を選択">
+            <option>React</option>
+            <option>TypeScript</option>
+            <option>GitHub</option>
+          </Select>
+        </FormControl>
+        <FormControl mb={4}>
+          <FormLabel fontWeight={"bold"}>GitHub ID</FormLabel>
+          <Input type="name" />
+          <FormLabel fontWeight={"bold"}>Qiita ID</FormLabel>
+          <Input type="name" />
+          <FormLabel fontWeight={"bold"}>X ID</FormLabel>
+          <Input type="name" />
+        </FormControl>
+        <Button colorScheme="blue">登録</Button>
+      </Box>
     </>
   );
 };
